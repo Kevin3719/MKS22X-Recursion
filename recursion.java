782910c7ea0e6978367fcd2c17915ae60e6b3965
Kevin Chen <kchen01@stuy.edu>
2019-02-03 19:09:12 -0500

    added sqrt method, still needs to be tested

--- recursion.java
+++ recursion.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class recursion{
     /*You may write additional private methods */
 
@@ -7,14 +8,13 @@ public class recursion{
 
     */
     public static double sqrt(double n, double tolerance){
-
+      return helpersprt(n, 1, tolerance);
     }
     public static double helpersprt(double n, double guess, double tolerance) {
-      if ()
-
-
-
-
+      if (tolerance < guess / n || tolerance < n / guess) {
+        return guess;
+      }
+      return helpersprt(n, ((n / guess) + guess) / 2, tolerance);
     }
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 0
@@ -23,11 +23,12 @@ public class recursion{
      *precondition: n is non-negative
      */
     public static int fib(int n){
-
+      return 0;
     }
 
     /*As Per classwork*/
     public static ArrayList<Integer> makeAllSums(int n){
+      return null;
     }
 
 }
